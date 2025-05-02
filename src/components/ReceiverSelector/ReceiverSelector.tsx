e06d987ebab12af581ee7eb85665427084815bf7
Patrick Muhire <patrickmuhire@92:e9:45:0c:2d:ed.home>
2025-05-01 21:31:41 -0400

    Complete receiver update

--- src/components/ReceiverSelector/ReceiverSelector.tsx
+++ src/components/ReceiverSelector/ReceiverSelector.tsx
@@ -110,22 +110,21 @@ const ContactSelector: React.FC<ContactSelectorProps> = ({
             >
               <div className="contact-info">
                 <div className="flag-container">
-                  {contact.country === "CA" && (
-                    <div className="canadian-flag">
-                      <div className="flag-red"></div>
-                      <div className="flag-white"></div>
-                      <div className="maple-leaf">
-                        <svg
-                          viewBox="0 0 50 50"
-                          fill="currentColor"
-                          width="16"
-                          height="16"
-                        >
-                          <path d="M25,0 L30,20 L50,20 L35,32 L42,50 L25,38 L8,50 L15,32 L0,20 L20,20 Z" />
-                        </svg>
-                      </div>
+                  {/* // get contryflag from receiver object */}
+                  <div className="canadian-flag">
+                    <div className="flag-red"></div>
+                    <div className="flag-white"></div>
+                    <div className="maple-leaf">
+                      <svg
+                        viewBox="0 0 50 50"
+                        fill="currentColor"
+                        width="16"
+                        height="16"
+                      >
+                        <path d="M25,0 L30,20 L50,20 L35,32 L42,50 L25,38 L8,50 L15,32 L0,20 L20,20 Z" />
+                      </svg>
                     </div>
-                  )}
+                  </div>
                 </div>
                 <div className="contact-details">
                   <div className="contact-name">{contact.name}</div>
